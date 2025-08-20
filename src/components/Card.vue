78472c55b88124bd7d21a00aa23d69e1d6b3a099
Italy-FBI <zonzino07@gmail.com>
2025-08-20 23:44:09 +0200

    start

---
+++ src/components/Card.vue
@@ -0,0 +1,23 @@
+<script setup>
+    const props = defineProps({
+        titolo: String,
+        obbiettivo: Number
+    })
+</script>
+<style>
+#car:hover{
+    border: 1px solid blue;
+}
+</style>
+<template>
+    <v-col>
+        <v-card id="car">
+            <v-card-text>
+                <h3>{{ props.titolo }}</h3>
+            </v-card-text>
+            <v-card-text v-if="props.obbiettivo">
+                {{ props.obbiet }}€
+            </v-card-text>
+        </v-card>
+    </v-col>
+</template>
